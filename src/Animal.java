f121dfdae5a8e8dbba10603a651c3f78d3203359
hanennn <162971774+hanennn@users.noreply.github.com>
2025-09-27 14:01:31 +0100

    Add files via upload

---
+++ src/Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    String family;
+    String name;
+    int age;
+    boolean isMammal;
+    public Animal(String s){}
+    // Constructeur paramétré
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+    @Override
+    public String toString() {
+        return "Animal [Famille=" + family + ", Nom=" + name + ", Âge=" + age + ", Mammifère=" + isMammal + "]";
+    }
+
+}
