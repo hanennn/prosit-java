73fca0ee0eb220238b4e40e1b2f70787d3370359
hanennn <162971774+hanennn@users.noreply.github.com>
2025-10-17 21:21:30 +0100

    Update ZooManagement.java

--- Prosit 5/src/tn/esprit/gestionZoo/entities/ZooManagement.java
+++ Prosit 5/src/tn/esprit/gestionZoo/entities/ZooManagement.java
@@ -22,25 +22,23 @@ public class ZooManagement {
 
         System.out.println(zooName + " comporte " + nbrCages + " cages.");
         //Prosit 2
-        Animal lion = new Animal("Animal" + (i + 1));
-        lion.family = "Félidé";
-        lion.name = "Lion";
-        lion.age = 5;
-        lion.isMammal = true;
+        Animal lion = new Animal("Félidé", "Lion", 5, true); // constructeur paramétré
 
         Zoo myZoo = new Zoo();
-        myZoo.name = "Parc Animalier";
-        myZoo.city = "Tunis";
-        // myZoo.NBR_CGES = 20;
+        myZoo.setNom("Parc Animalier");
+        myZoo.setCity("Tunis");
 
-        System.out.println("Zoo : " + myZoo.getNom() + " à " + myZoo.city);
+        System.out.println("Animal : " + lion);
+        System.out.println("Zoo : " + myZoo);
+
+        System.out.println("Zoo : " + myZoo.getNom() + " à " + myZoo.getCity());
         System.out.println("Animal : " + lion.getNom() + " (" + lion.getfamily() + ")");
 
         Animal lion1 = new Animal("Félidé", "Lion", 5, true);
         Zoo myZoo1 = new Zoo("Parc Animalier", "Tunis", 20);
 
         System.out.println("Animal créé : " + lion1.getNom());
-        System.out.println("Zoo créé : " + myZoo1.getNom() + " (" + myZoo.city + ")");
+        System.out.println("Zoo créé : " + myZoo1.getNom() + " (" + myZoo.getCity()+ ")");
 
         myZoo.displayZoo();
         System.out.println(myZoo);
